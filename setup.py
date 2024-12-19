47b95fcaf58dbce2e79aa62d07a11db2d3de8007
Bennett Schwartz <gc@gustycube.xyz>
2024-12-18 19:15:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="EasilyAI",
-    version="0.1.4",
+    version="0.1.6",
     description="A library that simplifies the usage of AI!",
     author="GustyCube",
     author_email="gc@gustycube.xyz",
